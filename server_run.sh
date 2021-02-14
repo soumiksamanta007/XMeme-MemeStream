81dd40538b8154ae6f436a0effcfcb65bbf02a15
soumiksamanta007 <soumiksamantaa@gmail.com>
2021-02-14 19:25:14 +0530

    modified server_run.sh

--- server_run.sh
+++ server_run.sh
@@ -4,14 +4,14 @@
 cd ./xmeme-backend
 
 # Setup DB or any other environment variables
-mysql -u root -pyour_password < ./db/create_meme_db.sql
 cat << EOF > .env
 PORT = 8081
 # DATABASE CONFIG
 DB_PATH=./db/memes.db
 EOF
 
-# run server
 npm install
 node ./db/db.js
+
+# run server
 npm start
